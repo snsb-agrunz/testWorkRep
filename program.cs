34e43ec7e6bd9216c438ea5115a56f5442d02569
ariane <arian@snsb>
2024-04-15 13:31:40 +0200

    new public version 1.2

---
+++ program.cs
@@ -0,0 +1,18 @@
+﻿namespace Test
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            
+            Console.WriteLine("dasfdsaf");
+        }
+
+        private static void WriteHeadingToScreen()
+        {
+            Console.WriteLine("Header for Tkik");
+            Console.WriteLine("-----------dsafd----");
+            Console.WriteLine();
+        }
+    }
+}
