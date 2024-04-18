c33398c0579095a9bcf853d4dd7ca24ce4305282
ariane <arian@snsb>
2024-04-18 10:54:59 +0200

    squash merge dieses commt wird angezeigt

--- program.cs
+++ program.cs
@@ -8,8 +8,15 @@
             Console.WriteLine("-----fasdlkfjsldakjflksdajflasjdfd----");
             Console.WriteLine();
             Console.WriteLine();
+            Console.WriteLine(dafsdfasdfa);
+            Console.WriteLine(dafdfasdf);
+            Console.WriteLine();
+            Console.WriteLine("Header for Tkik");
+            Console.WriteLine("-----fasdlkfjsldakjflksdajflasjdfd----");
             Console.WriteLine();
             Console.WriteLine();
+            Console.WriteLine(dafsdfasdfa);
+            Console.WriteLine(dafdfasdf);
             Console.WriteLine();
         }
     }
